6e3799263f11f376656df56a5e08ad3ac8d7315c
Lauren <43707274+Lorn12@users.noreply.github.com>
2024-01-18 16:34:47 -0600

    Creating functions to collect various weather data

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import Forecast from "./components/Forecast";
 import getWeatherData from "./services/weatherService";
+import getFormattedWeatherData from "./services/weatherService";
 // import UilReact from "@iconscout/react-unicons/icons/uil-react";
 
 function App() {
   const fetchWeather = async () => {
-    const data = await getWeatherData("weather", { q: "london" });
+    const data = await getFormattedWeatherData({ q: "london" });
     console.log(data);
   };
 
   fetchWeather();
+
   return (
-    <div className="mx-auto max-w-screen-md mt-20 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 rounded-lg">
+    <div className="mx-auto max-w-screen-md mt-20 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 ">
       <TopButtons />
       <Inputs />
       <TimeAndLocation />
